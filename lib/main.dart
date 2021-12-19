0aa0d09254dfe92b1b663ab1a42aa665e65c31d2
CypheX007 <yusufemindemirci061834@gmail.com>
2021-12-19 21:26:02 +0300

    changed first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:lets_head_out/Screens/Home.dart';
 import 'package:lets_head_out/Screens/Splash.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'Screens/Home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var status = prefs.getBool('isLoggedIn') ?? false;
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: status == true ? Home() : SplashScreen(),
+    home: SplashScreen(),
     title: 'Travel App design',
   ));
 }
