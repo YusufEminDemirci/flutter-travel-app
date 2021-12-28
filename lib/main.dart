051e20dd46930e538cb253b2e1c307641226e864
CypheX007 <yusufemindemirci061834@gmail.com>
2021-12-28 22:02:27 +0300

    added: status bar color changer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lets_head_out/Screens/Splash.dart';
+import 'package:lets_head_out/Utils/consts.dart';
 
 Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: mainColor,
+  ));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
