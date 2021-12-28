9c94bb55385ed20a4e4222cb7e0c81b5575d1cfb
CypheX007 <yusufemindemirci061834@gmail.com>
2021-12-28 22:19:00 +0300

    deleted unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lets_head_out/Screens/Splash.dart';
-import 'package:lets_head_out/Utils/consts.dart';
 
 Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
