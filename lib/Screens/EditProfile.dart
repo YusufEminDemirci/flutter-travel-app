b3144bc7dedac04c2a4bc860bc11234648e5482a
CypheX007 <yusufemindemirci061834@gmail.com>
2022-05-31 22:48:48 +0300

    redesigned edit profile page

--- lib/Screens/EditProfile.dart
+++ lib/Screens/EditProfile.dart
@@ -1,6 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:material_dialogs/material_dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:travel_food/Screens/Profile.dart';
+import 'package:travel_food/Utils/Buttons.dart';
 import 'package:travel_food/Utils/TextStyles.dart';
 import 'package:travel_food/Utils/consts.dart';
 
@@ -9,7 +14,18 @@ class EditProfile extends StatefulWidget {
   _EditProfile createState() => new _EditProfile();
 }
 
+final TextEditingController _passwordController = TextEditingController();
+final TextEditingController _passwordCheckController = TextEditingController();
+bool _obscureText = true;
+bool _obscureCheckText = true;
+
 class _EditProfile extends State<EditProfile> {
+  Future<void> initState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getString('userImageUrl');
+    prefs.getString('userPassword');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,54 +39,182 @@ class _EditProfile extends State<EditProfile> {
           centerTitle: true,
           elevation: 0.0,
           automaticallyImplyLeading: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                FontAwesomeIcons.check,
-                color: dayMainColor,
-              ),
-              onPressed: () {
-                //TODO: SAVE EDITED PROFILE !!!
-
-                FirebaseFirestore.instance
-                    .collection("Cities")
-                    .doc("bwHzr7Tg3E8ceJPE6ccU")
-                    .collection("Places")
-                    .doc("jyrDJRrRsauF1RRdOwqB")
-                    .set({
-                  "Hours": {
-                    "Friday": "11:00 - 02:00",
-                    "Monday": "11:00 - 02:00",
-                    "Saturday": "11:00 - 02:00",
-                    "Sunday": "11:00 - 02:00",
-                    "Thursday": "11:00 - 02:00",
-                    "Tuesday": "11:00 - 02:00",
-                    "Wednesday": "11:00 - 02:00",
-                  },
-                  "imageUrl":
-                      "https://firebasestorage.googleapis.com/v0/b/projectx-b164c.appspot.com/o/midyeci-ahmet.jpg?alt=media&token=36859e96-0abb-4ec6-b574-9101a17c73df",
-                  "description": "Midyeci",
-                  "id": "jyrDJRrRsauF1RRdOwqB",
-                  "location": "bwHzr7Tg3E8ceJPE6ccU",
-                  "name": "Midyeci Ahmet",
-                  "rate": "3.1",
-                  "telephone": "02324044556",
-                  "type": "place",
-                  "whoSee": {},
-                  "latitude": "38.4353292",
-                  "longitude": "27.145659",
-                });
-              },
-            )
-          ],
         ),
       ),
       body: Container(
         alignment: Alignment.center,
-        //TODO: EDIT PROFILE !!!
-        child: Text(
-          "Coming soon...",
-          style: TextStyle(fontSize: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 340.0,
+              child: TextFormField(
+                style: TextStyle(
+                    fontFamily: "nunito",
+                    fontWeight: FontWeight.w500,
+                    color: kgreyDark,
+                    fontSize: 15.5),
+                obscureText: _obscureText,
+                keyboardType: TextInputType.text,
+                controller: _passwordController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'This Field Cannot Be Left Blank !';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.grey.shade700,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                    icon: _obscureText
+                        ? Icon(
+                            FontAwesomeIcons.eye,
+                            color: Colors.grey.shade700,
+                          )
+                        : Icon(
+                            FontAwesomeIcons.eyeSlash,
+                            color: Colors.grey.shade700,
+                          ),
+                    iconSize: 20,
+                  ),
+                  hintText: "New Password",
+                  hintStyle: TextStyle(
+                      fontFamily: "nunito",
+                      fontWeight: FontWeight.w500,
+                      color: kgreyDark,
+                      fontSize: 15.5),
+                  contentPadding:
+                      const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+                  focusColor: Colors.grey.shade700,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: 340.0,
+              child: TextFormField(
+                style: TextStyle(
+                    fontFamily: "nunito",
+                    fontWeight: FontWeight.w500,
+                    color: kgreyDark,
+                    fontSize: 15.5),
+                obscureText: _obscureCheckText,
+                keyboardType: TextInputType.text,
+                controller: _passwordCheckController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'This Field Cannot Be Left Blank !';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.grey.shade700,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscureCheckText = !_obscureCheckText;
+                      });
+                    },
+                    icon: _obscureCheckText
+                        ? Icon(
+                            FontAwesomeIcons.eye,
+                            color: Colors.grey.shade700,
+                          )
+                        : Icon(
+                            FontAwesomeIcons.eyeSlash,
+                            color: Colors.grey.shade700,
+                          ),
+                    iconSize: 20,
+                  ),
+                  hintText: "New Password (Control)",
+                  hintStyle: TextStyle(
+                      fontFamily: "nunito",
+                      fontWeight: FontWeight.w500,
+                      color: kgreyDark,
+                      fontSize: 15.5),
+                  contentPadding:
+                      const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+                  focusColor: Colors.grey.shade700,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            WideButton.bold("Save", () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+
+              FirebaseFirestore.instance
+                  .collection("Users")
+                  .get()
+                  .then((querySnapshot) {
+                querySnapshot.docs.forEach((result) {
+                  String _email = result.data()["e-mail"];
+                  String _password = result.data()["password"];
+
+                  String _name = result.data()["name"];
+                  String _surname = result.data()["surname"];
+                  String _imageUrl = result.data()["imageUrl"];
+                  String _id = result.data()["id"];
+
+                  prefs.setString('userEmail', _email);
+                  prefs.setString('userName', _name);
+                  prefs.setString('userSurname', _surname);
+                  prefs.setString('userImageUrl', _imageUrl);
+                  prefs.setString('userId', _id);
+                  prefs.setString('userPassword', _password);
+
+                  prefs.setBool('isLoggedIn', true);
+
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => new Profile()),
+                      ModalRoute.withName("/Home"));
+                });
+              });
+              if (_passwordController.text == _passwordCheckController.text) {
+                prefs.setString('userPassword', _passwordController.text);
+              }
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => new Profile()),
+                  ModalRoute.withName("/Home"));
+            }, true),
+          ],
         ),
       ),
     );
